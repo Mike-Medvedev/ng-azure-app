e56357ff633ef231f2b9256fa16b2a896eaf63c7
Mike Medvedev <mmedvedev20@gmail.com>
2024-06-10 14:01:06 -0400

    changed env for prod

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  client_id: '662942defdda40758c08f490b89fce4b',
-  client_secret: 'df296bd8ac0d4c689332eb6d2c857b2d',
-  redirect_url: 'http://localhost:4200/callback',
-  api_url: 'http://localhost:3000',
+  client_id: "662942defdda40758c08f490b89fce4b",
+  client_secret: "df296bd8ac0d4c689332eb6d2c857b2d",
+  redirect_url: "https://polite-sky-0e426660f.5.azurestaticapps.net/callback",
+  api_url: "http://ng-azure-api.azurewebsites.net/",
 };
