33450ed72abbe38517080f9a20a7c1c9e39df4e8
Mike Medvedev <mmedvedev20@gmail.com>
2024-06-11 14:21:18 -0400

    api is https now

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -3,5 +3,5 @@ export const environment = {
   client_id: "662942defdda40758c08f490b89fce4b",
   client_secret: "df296bd8ac0d4c689332eb6d2c857b2d",
   redirect_url: "https://polite-sky-0e426660f.5.azurestaticapps.net/callback",
-  api_url: "http://ng-azure-api.azurewebsites.net",
+  api_url: "https://ng-azure-api.azurewebsites.net",
 };
